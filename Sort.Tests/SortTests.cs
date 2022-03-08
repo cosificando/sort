6cb4dea5a799b6d9e7e80a830eb4be9de63fca20
Pablo Escribano de Loza <pablo.escribano@gmail.com>
2022-03-08 17:13:34 +0100

    + add initial files

---
+++ Sort.Tests/SortTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Sort.Tests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false,"Not implemented yet");
+        }
+    }
+}
