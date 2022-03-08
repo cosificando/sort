980499bb1e07f261c6f3e17cc98928f76ca01447
Pablo Escribano de Loza <pablo.escribano@gmail.com>
2022-03-08 17:34:47 +0100

    + add two new tests

--- Sort.Tests/SortTests.cs
+++ Sort.Tests/SortTests.cs
@@ -1,14 +1,33 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Sort.Tests
 {
     public class SortTests
     {
+        // Ordenar una Matrix vacía
         [Fact]
-        public void Test1()
+        public void OrdenamosUnaListaVaciaElResultadoEsUnaListaVacia()
         {
-            Assert.True(false,"Not implemented yet");
+
+            List<int> list = new List<int>();
+            List<int> sortList = Sort.asList(list);
+            
+            Assert.True(list.Count == 0);
+            Assert.True(sortList.Count == 0);
+        }
+        // La ordenación de una Lista con un elemento es la misma lista con un elemento
+        [Fact]
+        public void OrdenadomosUnaListaConUnElementoEsLaMismaListaConUnElemento()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            List<int> sortList = Sort.asList(list);
+            Assert.True(list.Count == 1);
+            Assert.True(sortList.Count == 1);
+            Assert.True(list.ToArray().GetValue(0) == sortList.ToArray().GetValue(0));
+
         }
     }
 }
--- Sort/Sort.cs
+++ Sort/Sort.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sort
 {
     public class Sort
     {
+        public static List<int> asList(List<int> list)
+        {
+            return new List<int>();
+        }
     }
 }
